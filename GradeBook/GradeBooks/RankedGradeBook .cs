feacb633a386ff8ac1ba95d15ac820363bbd40cf
Diana <diana9195@gmail.com>
2019-10-21 16:56:53 +0300

    added task 1

---
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        private static readonly GradeBookType Ranked;
+
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = RankedGradeBook.Ranked;
+        }
+
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        private static readonly GradeBookType Standard;
+
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = StandardGradeBook.Standard;
+        }
+    }
+}
