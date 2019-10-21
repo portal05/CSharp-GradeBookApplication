6c416f8af770d9255f10562f7c23dbcdc6ed2a63
Diana <diana9195@gmail.com>
2019-10-21 20:23:30 +0300

    3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -13,6 +14,44 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);//20% of the student count
+            var grades = Students.OrderByDescending(e=> e.AverageGrade).Select(e=> e.AverageGrade).ToList();
+           
+            if (grades[threshold - 1]<= averageGrade)
+                return 'A';
+            else if(grades[(threshold*2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold*3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold*4)- 1] <= averageGrade)
+                return 'D';
+
+            return 'F';
+
+        }
+/*
+        public double[] OrderByDescendingAverageGrade()
+        {
+            double[] gradeList = new double[Students.Count];
+           // double maxGrade = double.MinValue;
+            var i = 0;
+            foreach (var student in Students)
+            {
+                gradeList[i++] = student.AverageGrade;
+            }
+            // Sort array in ascending order. 
+            Array.Sort(gradeList);
+
+            // reverse array 
+            Array.Reverse(gradeList);
+
+            return gradeList;
+        }
+        */
     }
 }
